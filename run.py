290970e7ce004a500c5550a964c4d824ecfbe022
WaqasWikky <155289834+WaqasWikky@users.noreply.github.com>
2025-08-09 20:55:22 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,29 @@
 from honeypot import ssh_honey, http_honey
 
-print("""
+while True:
+    print("""
 [1] Run SSH Honeypot
 [2] Run HTTP Honeypot
+[0] Exit
 """)
 
-choice = input("Select honeypot to start: ")
+    choice = input("Select honeypot to start: ")
 
-if choice == "1":
-    ssh_honey.start_ssh_honeypot()
-elif choice == "2":
-    http_honey.start_http_honeypot()
-else:
-    print("Invalid option.")
+    if choice == "1":
+        try:
+            ssh_honey.start_ssh_honeypot()
+        except KeyboardInterrupt:
+            print("\n[!] SSH Honeypot stopped. Returning to menu...\n")
+
+    elif choice == "2":
+        try:
+            http_honey.start_http_honeypot()
+        except KeyboardInterrupt:
+            print("\n[!] HTTP Honeypot stopped. Returning to menu...\n")
+
+    elif choice == "0":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid option. Please try again.")
